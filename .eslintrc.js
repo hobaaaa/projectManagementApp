91751a25d2a99bc5d38288fe18dc5766c4fe312c
hobaaaa <denizgokbudak@gmail.com>
2025-06-20 00:57:19 +0300

    type bug fix2

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-non-null-asserted-optional-chain": "warn",
   },
